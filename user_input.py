a67848f8127763106547c5fa41e0d24e94341716
Tina-chris <christineanyango112@gmail.com>
2024-08-28 23:26:19 +0300

    user_input.py file

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Ask the user for their name and store it in a variable called "name"
+name = input("Please enter your name: Tina")
+
+# Ask the user for their age and store it in a variable called "age"
+age = input("Please enter your age: 23")
+
+# Ask the user for their location and store it in a variable called "location"
+location = input("Please enter your location: Nairobi")
+
+# Print out a personalized message using the user's name, age, and location
+print(f"Hello Tina, you are 23 years old and live in Nairobi.")
